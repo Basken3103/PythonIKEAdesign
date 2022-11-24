5dd8c4ef139d56edab178e59d7b2288a7ea09eda
Christopher S. Vesterby <vesterby@hotmail.dk>
2022-11-24 14:08:05 +0100

    Create IKEAdesign.py

---
+++ IKEAdesign.py
@@ -0,0 +1,18 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+blue = (0,0,255)
+
+yellow = (255, 255, 0)
+
+
+sense.show_message("C",text_colour=blue, back_colour= yellow)
+
+r = 0
+g = 0
+b = 0
+y = 0
+m = 0
+c = 0
+
+sense.clear((r, g, b))
